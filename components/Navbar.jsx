eea9a16d1f256977caa1e6f0be990f773da65b43
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-09-13 00:42:55 +0530

    minor changes and gsap added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import RegisterBtn from './RegisterBtn'
 
-function Navbars() {
+function Navbar() {
   return (
     <div className='h-[4.5rem] w-max-full flex justify-between rounded-[10px] border-[#6A9C89] border-2 p-2 backdrop-blur-sm fixed top-0 left-0 right-0 z-50 lg:mx-40 lg:my-5'>
       <div className='text-md text-white flex items-center font-[Tasa-SemiBold]'>
@@ -13,8 +13,8 @@ function Navbars() {
       </div>
 
       <div className='flex'>
-        <img src="./iemLogo.webp" alt="iemLogo" className='lg:mr-7' />
-        <img src="./innovacionLogo.webp" alt="InnovacionLogo" />
+        <img src="/public/iemLogo.webp" alt="iemLogo" className='lg:mr-7' />
+        <img src="/public/innovacionLogo.webp" alt="InnovacionLogo" />
       </div>
 
       <RegisterBtn />
@@ -22,4 +22,4 @@ function Navbars() {
   )
 }
 
-export default Navbars
+export default Navbar
