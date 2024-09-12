4ac91a48f1c364a49b499d9356ee03f84b26d65d
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-09-12 23:56:55 +0530

    event page done with cards

--- components/ui/focus-cards.jsx
+++ components/ui/focus-cards.jsx
@@ -14,7 +14,7 @@ export const Card = React.memo(
       onMouseEnter={() => setHovered(index)}
       onMouseLeave={() => setHovered(null)}
       className={cn(
-        "rounded-lg relative bg-gray-100 dark:bg-neutral-900 overflow-hidden h-60 md:h-96 w-full transition-all duration-300 ease-out",
+        "rounded-lg relative dark:bg-neutral-900 overflow-hidden h-60 md:h-96 w-full transition-all duration-300 ease-out",
         hovered !== null && hovered !== index && "blur-sm scale-[0.98]"
       )}
     >
@@ -22,7 +22,7 @@ export const Card = React.memo(
         src={card.src}
         alt={card.title}
         fill
-        className="object-cover absolute inset-0"
+        className="object-cover absolute inset-0 border-2 border-[#16423C] rounded-[10px]"
       />
       <div
         className={cn(
@@ -45,7 +45,7 @@ export function FocusCards({ cards }) {
   const [hovered, setHovered] = useState(null);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-5xl mx-auto md:px-8 w-full">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-5xl w-full">
       {cards.map((card, index) => (
         <Card
           key={card.title}
@@ -53,6 +53,7 @@ export function FocusCards({ cards }) {
           index={index}
           hovered={hovered}
           setHovered={setHovered}
+
         />
       ))}
     </div>
