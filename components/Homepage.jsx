5e1381fc44d8fb243508f705c08d6c026ccb6c51
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-09-12 22:34:47 +0530

    homepage fix

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import RegisterBtn from './RegisterBtn'
-
+import StarryBackground from './StarryBackground/StarryBackground'
 
 function Homepage() {
   return (
     // Home page
-    <div className="h-full flex justify-center items-center flex-col pb-36">
-    <h1 className="text-8xl text-white font-[Quantum] pb-11">INNOVACIÓN</h1>
-    <RegisterBtn />
+    <div className="h-screen flex justify-center items-center flex-col pb-36 lg:mx-40 lg:py-5">
+      <StarryBackground extraClass="bg-[radial-gradient(circle_at_bottom,#16423C_0%,#000_100%)]" />
+      {/* <StarryBackground /> */}
+      <h1 className="text-8xl text-white font-[Quantum] pb-11">INNOVACIÓN</h1>
+      <RegisterBtn />
     </div>
   )
 }
