dbef1acd3c2a576583085de27e9af482050efdff
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-09-12 22:10:56 +0530

    navbar fix

--- components/Navbars.jsx
+++ components/Navbars.jsx
@@ -5,22 +5,22 @@ import RegisterBtn from './RegisterBtn'
 
 function Navbars() {
   return (
-    <div className='h-[4.5rem] w-full flex justify-between rounded-[10px] border-[#6A9C89] border-2 p-2 backdrop-blur-sm'>
-    <div className='text-md text-white flex items-center font-[Tasa-SemiBold]'>
+    <div className='h-[4.5rem] w-max-full flex justify-between rounded-[10px] border-[#6A9C89] border-2 p-2 backdrop-blur-sm fixed top-0 left-0 right-0 z-50 lg:mx-40 lg:my-5'>
+      <div className='text-md text-white flex items-center font-[Tasa-SemiBold]'>
         <span className='lg:mr-5 lg:ml-5'>Home</span>
         <span className='lg:mr-5'>Event</span>
         <span className='lg:mr-5'>Gallery</span>
         <span className='lg:mr-5'>Partners</span>
         <span>Contact Us</span>
-    </div>
+      </div>
 
-    <div className='flex'>
-        <img src='/iemLogo.webp' alt="iemLogo" className='lg:mr-7' />
-        <img src='/innovacionLogo.webp' alt="InnovacionLogo" />
-    </div>
+      <div className='flex'>
+        <img src={IemLogo} alt="iemLogo" className='lg:mr-7' />
+        <img src={InnovacionLogo} alt="InnovacionLogo" />
+      </div>
 
-    <RegisterBtn />
-  </div>
+      <RegisterBtn />
+    </div>
   )
 }
 
