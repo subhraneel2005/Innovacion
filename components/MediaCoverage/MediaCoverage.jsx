dd40a41aa33bccee9ab8f65d126df045d73e5eb9
Arkaprobho Laha <arkaprobholaha428@gmail.com>
2024-09-27 03:39:06 +0530

    partners logo add on

--- components/MediaCoverage/MediaCoverage.jsx
+++ components/MediaCoverage/MediaCoverage.jsx
@@ -36,7 +36,7 @@ function MediaCoverage() {
 
 
       <div className="w-full flex flex-col items-center justify-center relative z-20">
-        <h2 className="text-4xl md:text-[3.2rem] text-gray-100 font-[Tasa-SemiBold] mb-4">Partners</h2>
+        <h2 className="text-4xl md:text-[3.2rem] text-gray-100 font-[Tasa-SemiBold] mb-4">Previous Year Partners</h2>
         <MarqueeDemo />
       </div>
     </div>
--- components/Partners.jsx
+++ components/Partners.jsx
@@ -3,28 +3,68 @@ import Marquee from "@/components/magicui/marquee";
 
 const reviews = [
   {
-    name: "Jack",
-    img: "https://avatar.vercel.sh/jack",
+    name: "Rotary Club",
+    img: "partners_logo/1.png",
   },
   {
     name: "Jill",
-    img: "https://avatar.vercel.sh/jill",
+    img: "partners_logo/2.png",
   },
   {
     name: "John",
-    img: "https://avatar.vercel.sh/john",
+    img: "partners_logo/3.png",
   },
   {
     name: "Jane",
-    img: "https://avatar.vercel.sh/jane",
+    img: "partners_logo/4.png",
   },
   {
     name: "Jenny",
-    img: "https://avatar.vercel.sh/jenny",
+    img: "partners_logo/5.png",
   },
   {
     name: "James",
-    img: "https://avatar.vercel.sh/james",
+    img: "partners_logo/6.png",
+  },
+  {
+    name: "Rotary Club",
+    img: "partners_logo/7.png",
+  },
+  {
+    name: "Rotary Club",
+    img: "partners_logo/8.png",
+  },
+  {
+    name: "Rotary Club",
+    img: "partners_logo/9.png",
+  },
+  {
+    name: "Rotary Club",
+    img: "partners_logo/10.png",
+  },
+  {
+    name: "Rotary Club",
+    img: "partners_logo/11.png",
+  },
+  {
+    name: "Rotary Club",
+    img: "partners_logo/12.png",
+  },
+  {
+    name: "Rotary Club",
+    img: "partners_logo/13.png",
+  },
+  {
+    name: "Rotary Club",
+    img: "partners_logo/14.png",
+  },
+  {
+    name: "Rotary Club",
+    img: "partners_logo/15.png",
+  },
+  {
+    name: "Rotary Club",
+    img: "partners_logo/16.png",
   },
 ];
 
@@ -40,7 +80,7 @@ const ReviewCard = ({ img, name }) => {
     >
       <div className="flex flex-col items-center justify-center ">
         <img src={img} alt={name} className="w-32 h-32 md:w-40 md:h-40" />
-        <h1 className="text-gray-100 font-[Tasa-SemiBold] text-2xl mt-4">Brand</h1>
+        <h1 className="text-gray-100 font-[Tasa-SemiBold] text-2xl mt-4"></h1>
       </div>
     </figure>
   );
