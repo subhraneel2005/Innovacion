bff93daa3c30c726c3e5a7881c24357e663b8b69
Arkaprobho Laha <arkaprobholaha428@gmail.com>
2024-09-28 14:38:19 +0530

    minor changes done
    Individual event descriptions added.
    "brand" removed from partner logos
    1 additional event on Management that has been removed.
    Logo of Smart Society USA has been changed.

--- components/Events.jsx
+++ components/Events.jsx
@@ -9,7 +9,7 @@ function Events() {
       <StarryBackground extraClass={" z-10 top-0 left-0 right-0 bottom-0 rounded-[40px] bg-[radial-gradient(circle_at_bottom,#000_0%,#000_100%)]"} />
 
       <GradientBox extraClass="mb-8 flex justify-center items-center flex-col my-[100px] mx-[50px]">
-        <h2 className="text-4xl md:text-[3.2rem] text-center font-[Tasa-SemiBold] mb-10 text-white">About Innocavión</h2>
+        <h2 className="text-4xl md:text-[3.2rem] text-center font-[Tasa-SemiBold] mb-10 text-white">About Innovación</h2>
         <p className="text-xs md:text-base font-[Tasa-Regular] leading-6 text-gray-300 text-center">
           Institute of Engineering and Management, Kolkata in association with University of Engineering and Management, proudly presents the 12th edition of its Annual Techno-Management Fest, Innovación 2025, from 7th to 9th March, 2025. Innovación ignites intellects like celestial supernovas, a magnificent fusion of cutting-Edge technology and boundless creativity. Students will showcase their unparalleled brilliance, their ideas shining like stars in a limitless universe of innovation. Each day unfolds into a grand celebration, where invention soars to new dimensions. Embark on this stellar voyage and witness the uncharted realms of ingenuity and discovery!        </p>
       </GradientBox>
