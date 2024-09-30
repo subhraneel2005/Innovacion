dca0523e2c123eba5c42ba67d92942af3e68deaf
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-09-30 22:18:08 +0530

    events bug fixing

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,8 +1,11 @@
 import IndividualEvent from '@/components/IndividualEvent'
 import React from 'react'
+import { Suspense } from 'react';
 
 export default function event() {
   return (
-    <IndividualEvent/>
+    <Suspense fallback={<div>Loading...</div>}>
+      <IndividualEvent />
+    </Suspense>
   )
 }
