729d29f54544ea675aa562c93c1146ae09fa81e0
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-01 12:20:13 +0530

    minor change

--- components/ContactUs/ContactData.js
+++ components/ContactUs/ContactData.js
@@ -3,7 +3,7 @@ export const ContactData = [
         profilePic: "/ContactUs/sayanDey.webp",
         name: "Sayan Dey",
         department: "Accounts and Logistics",
-        phnNo: "+91 89618 389331"
+        phnNo: "+91 8961838933"
     },
     {
         profilePic: "/ContactUs/arkaprobhoLaha.webp",
@@ -15,18 +15,18 @@ export const ContactData = [
         profilePic: "/ContactUs/aniketChakraborty.webp",
         name: "Aniket Chakraborty",
         department: "Database and Verification",
-        phnNo: "+91 82408 15528"
+        phnNo: "+91 8240815528"
     },
     {
         profilePic: "/ContactUs/gargiJoarder.webp",
         name: "Gargi Joarder",
         department: "Social media and PR",
-        phnNo: "+91 81160 06315"
+        phnNo: "+91 8116006315"
     },
     {
         profilePic: "/ContactUs/sristikaMajumder.webp",
         name: "Sristika Majumder",
         department: "Planning and Execution, Floor Management",
-        phnNo: "+91 87777 48634"
+        phnNo: "+91 8777748634"
     },
 ]
