4ad6962c62aacc26b13d7770fb4982e58b2905b9
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-01 12:38:12 +0530

    contact us whatsapp link

--- components/ContactUs/ContactCard.jsx
+++ components/ContactUs/ContactCard.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import GradientBox from '../ui/GradientBox'
+import { FaWhatsapp } from 'react-icons/fa'
 
-const ContactCard = ({ profilePic, name, department, phnNo }) => {
+const ContactCard = ({ profilePic, name, department, phnNo, link }) => {
     return (
         <GradientBox extraClass="mb-8 flex items-center flex-col md:flex-row gap-6 p-6 md:p-4 ">
             <img
@@ -12,7 +13,10 @@ const ContactCard = ({ profilePic, name, department, phnNo }) => {
             <div className="text-center md:text-left">
                 <h1 className="text-2xl md:text-xl text-white font-bold mb-2 font-[Ethnocentric]">{name}</h1>
                 <h2 className="text-lg md:text-base text-gray-300 mb-2 font-[Megatrans]">{department}</h2>
-                <h1 className="text-xl md:text-xl text-white font-semibold text-primary-500">{phnNo}</h1>
+                <div className='flex'>
+                    <FaWhatsapp className='text-emerald-400 text-xl md:text-2xl mr-2 mt-[2px]' />
+                    <h1 className="text-xl md:text-xl text-white font-semibold text-primary-500"><a href={link}>{phnNo}</a></h1>
+                </div>
             </div>
         </GradientBox>
     )
