e47784fed66c0c1b595dea97a7a434ec2807c6ff
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-07 22:56:28 +0530

    homepage register button link

--- components/HomePage/Homepage.jsx
+++ components/HomePage/Homepage.jsx
@@ -2,8 +2,14 @@
 import React from 'react'
 import RegisterBtn from '../ui/RegisterBtn'
 import StarryBackground from '../StarryBackground/StarryBackground'
+import { useRouter } from 'next/navigation';
 
 function Homepage() {
+  const router = useRouter()
+
+  const navigateToEventInfo = () => {
+    router.push('/eventInfo')
+  }
   return (
     <div id='homepage' className="h-screen w-full relative flex justify-center items-center flex-col">
       <StarryBackground extraClass="bg-[radial-gradient(circle_at_bottom,#16423C_0%,#000_100%)]" />
@@ -12,7 +18,7 @@ function Homepage() {
         <h1 className="text-4xl md:text-7xl text-white font-[Ethnocentric] px-6">2025</h1>
         <img src="/Logo/logo.webp" alt="" className='w-32 md:w-64 mt-3 mb-4' />
       </div>
-      <RegisterBtn text="Register" extraClass="h-[48px]" />
+      <RegisterBtn text="Register" extraClass="h-[48px]" onClick={navigateToEventInfo} />
     </div>
   )
 }
