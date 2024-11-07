1370458a77ec4565d60fb28ed42b472b3347e4ad
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-07 22:54:15 +0530

    event bulletin layout fix

--- components/Events/EventBulletinCard.jsx
+++ components/Events/EventBulletinCard.jsx
@@ -11,14 +11,14 @@ const EventBulletinCard = ({ bannerPic, name, description }) => {
     }
 
     return (
-        <GradientBox extraClass="mb-8 flex justify-center  flex-row gap-6 p-6 md:p-4 ">
+        <GradientBox extraClass="mb-8 flex justify-center items-center flex-col md:flex-row gap-6 p-6 md:p-4 ">
             <img
                 src={bannerPic}
                 alt={name}
-                className="w-32 h-32 md:w-80 md:h-80 rounded-xl object-cover"
+                className="w-40 h-40 md:w-80 md:h-80 rounded-xl object-cover"
             />
-            <div className="text-center md:text-left">
-                <h1 className="text-2xl md:text-xl text-white font-bold mb-2 font-[Ethnocentric]">{name}</h1>
+            <div className="text-justify md:text-left">
+                <h1 className="text-2xl md:text-xl text-center md:text-left text-white font-bold mb-2 font-[Ethnocentric]">{name}</h1>
                 <h2 className="text-lg md:text-base text-gray-300 mb-2 font-[Megatrans]">{description}</h2>
                 <RegisterBtn text="Register" extraClass="mt-6 md:h-[48px]" onClick={navigateToEventPage} />
             </div>
