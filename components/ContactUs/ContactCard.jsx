1954be7a3f05874c2b18e8ea13c64008ac4028e8
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-11-08 22:19:32 +0530

    contact us card layout bug fix

--- components/ContactUs/ContactCard.jsx
+++ components/ContactUs/ContactCard.jsx
@@ -10,10 +10,10 @@ const ContactCard = ({ profilePic, name, department, phnNo, link }) => {
                 alt={name}
                 className="w-32 h-32 md:w-40 md:h-40 rounded-full object-cover"
             />
-            <div className="text-center md:text-left">
+            <div className="text-center flex flex-col items-center md:items-start md:text-left">
                 <h1 className="text-2xl md:text-xl text-white font-bold mb-2 font-[Ethnocentric]">{name}</h1>
                 <h2 className="text-lg md:text-base text-gray-300 mb-2 font-[Megatrans]">{department}</h2>
-                <div className='flex'>
+                <div className='flex items-center'>
                     <FaWhatsapp className='text-emerald-400 text-xl md:text-2xl mr-2 mt-[2px]' />
                     <h1 className="text-xl md:text-xl text-white font-semibold text-primary-500"><a href={link}>{phnNo}</a></h1>
                 </div>
